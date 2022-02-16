26da12545e1a897616480d06dbf93211162e3b28
Peter Tran <tran.peter08@gmail.com>
2022-02-16 06:48:26 -0800

    added body parsers

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // setup router
 app.use('/api', apiRouter);
