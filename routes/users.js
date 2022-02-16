3a57db1827b539dedb2e4eaf85631734e6111105
Peter Tran <tran.peter08@gmail.com>
2022-02-16 09:27:41 -0800

    fixed typo

--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,10 @@ const Exercise = require('../models/Exercise');
 
 router.get('/', async (req, res, next) => {
   try {
-    const users = await Users.find({}, { _id: 1, username: 1 });
+    const users = await User.find({}, { _id: 1, username: 1 });
     return res.json(users);
   } catch (error) {
+    console.log(error.message);
     next('error...');
   }
 });
